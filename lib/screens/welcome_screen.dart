41f7cfab36f9822ec3cccad74988fe721b1414a4
Sanjay-2000 <sanjay.space.66@gmail.com>
2021-03-27 12:31:17 +0530

    first commit

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,139 @@
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  static const String id = 'welcome_screen';
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation animation;
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(vsync: this, duration: Duration(seconds: 1));
+
+    animation = ColorTween(
+      begin: Colors.grey,
+      end: Colors.white,
+    ).animate(controller);
+    //this can be used for curved animations
+    // animation = CurvedAnimation(parent: controller, curve: Curves.decelerate);
+    // controller.forward();
+    // animation.addStatusListener((status) {
+    //   if (status == AnimationStatus.completed) {
+    //     controller.reverse(from: 1);
+    //   } else if (status == AnimationStatus.dismissed) {
+    //     controller.forward();
+    //   }
+    //   ;
+    //   //print(status);
+    // });
+    controller.forward();
+
+    controller.addListener(() {
+      setState(() {});
+      // print(controller.value);
+      //print(animation.value);
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: animation.value,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    child: Image.asset('images/logo.png'),
+                    height: 60,
+                  ),
+                ),
+                TyperAnimatedTextKit(
+                  text: ['Flash Chat'],
+                  totalRepeatCount: 1,
+                  speed: Duration(milliseconds: 200),
+                  textStyle: TextStyle(
+                    color: Colors.black,
+                    fontSize: 45.0,
+                    fontWeight: FontWeight.w900,
+                    //Text: ('Flash Chat'),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: Material(
+                elevation: 5.0,
+                color: Colors.lightBlueAccent,
+                borderRadius: BorderRadius.circular(30.0),
+                child: MaterialButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, LoginScreen.id);
+                    // the below code for un named routes
+                    // Navigator.push(context,
+                    //     MaterialPageRoute(builder: (context) {
+                    //   return LoginScreen();
+                    // }));
+                  },
+                  minWidth: 200.0,
+                  height: 42.0,
+                  child: Text(
+                    'Log In',
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: Material(
+                color: Colors.blueAccent,
+                borderRadius: BorderRadius.circular(30.0),
+                elevation: 6.0,
+                child: MaterialButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, RegistrationScreen.id);
+                    // the below code for un named routes
+                    // Navigator.push(context,
+                    //     MaterialPageRoute(builder: (context) {
+                    //   return RegistrationScreen();
+                    // }));
+                  },
+                  minWidth: 200.0,
+                  height: 42.0,
+                  child: Text(
+                    'Register',
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
